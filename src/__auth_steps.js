89ff67a5a070bcbe9758711011b9fd81cb32bd81
Tanimismat <tanim.ismat@gmail.com>
2022-04-18 01:30:36 +0600

    signup

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,9 +1,13 @@
 /** 
-*1.create project in firebase
-*2. npm install firebase
-*3. initialize firebase config file
-*4. export auth from firebase config file
-*5. enable email password auth
+    *1.create project in firebase
+    *2. npm install firebase
+    *3. initialize firebase config file
+    *4. export auth from firebase config file
+    *5. enable email password auth
+    6. create Login,Signup component, setup route
+    7. attach form field handler and form submit handler
+    8. npm install --save react-firebase-hooks
+    9. useCreateUserWithEmailAndPassword from react-firebase-hooks
 */
 
 
