e3721b6348f93133d360a90c7756676913080b23
Tanimismat <tanim.ismat@gmail.com>
2022-04-17 13:26:54 +0600

    steps

---
+++ src/__auth_steps.js
@@ -0,0 +1,18 @@
+/** 
+*1.create project in firebase
+*2. npm install firebase
+*3. initialize firebase config file
+*4. export auth from firebase config file
+*5. enable email password auth
+*/
+
+
+
+
+/**
+ * hosting steps
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 3. firebase init
+ * 4. firebase deploy
+*/
