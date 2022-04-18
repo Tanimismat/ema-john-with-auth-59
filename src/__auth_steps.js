b9ba4dd2558d93d6391a0659de9fb49ad013898c
Tanimismat <tanim.ismat@gmail.com>
2022-04-18 13:37:46 +0600

    firebase deploy steps

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,15 +1,26 @@
 /** 
     1. create project in firebase
+
     2. npm install firebase
+
     3. initialize firebase config file
+
     4. export auth from firebase config file
-    5. enable email password auth
+
+    5. enable email password auth or google
+
     6. create Login,Signup component, setup route
+
     7. attach form field handler and form submit handler
+
     8. npm install --save react-firebase-hooks
+
     9. useCreateUserWithEmailAndPassword from react-firebase-hooks
+
     10. if user is created redirect to the expected page(using useNavigate())
+
     11. useSignInWithEmailAndPassword for sign in
+
     12. create RequireAuth component to protect route
         --> destructuring children
         --> take user using useAuthState(auth)
@@ -18,7 +29,9 @@
                 -state =from:location (tracking user location) 
                 -replace
         --> returning children
+
     13. wrap route with RequireAuth in app.js
+
     14. since we are redirected to login page ,thats why 'form' is declared in login page
         --> useNavigate()
         --> useLocation()
@@ -32,9 +45,10 @@
 
 
 /**
- * hosting steps
- * 1. npm install -g firebase-tools
- * 2. firebase login
- * 3. firebase init
- * 4. firebase deploy
+ * Firebase hosting/deploy steps
+ * 1. npm install -g firebase-tools (one time for computer)
+ * 2. firebase login (one time for computer)
+ * 3. firebase init (one time for each project)
+ * 4. npm run build (every time when deploying)
+ * 5. firebase deploy (every time when deploying)
 */
